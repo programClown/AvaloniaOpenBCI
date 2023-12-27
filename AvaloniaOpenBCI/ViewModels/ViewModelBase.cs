51b99114adab40e67b20b103397ad35cdc4ab9e7
codeClown <zhaomengshou@126.com>
2023-12-27 10:48:40 +0800

    first commit

---
+++ AvaloniaOpenBCI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaOpenBCI.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AvaloniaOpenBCI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaOpenBCI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
